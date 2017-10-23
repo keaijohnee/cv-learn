2a006f60f66897f38bfc95943ab3fa5b3c9c08ed
19920625lsg <1648266192@qq.com>
2017-10-23 16:26:00 +0800

    看到第3讲了

---
+++ opencv4java/src/main/java/com/huawei/l00379880/OpenCV02Lena_Gray.java
@@ -0,0 +1,21 @@
+package com.huawei.l00379880;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+
+public class OpenCV02Lena_Gray {
+    public static void main(String[] args) {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        // 以灰度图的形式加载,CV_LOAD_IMAGE_GRAYSCALE
+        Mat src = Imgcodecs.imread("D:\\l00379880\\GithubProjects\\images\\lena.jpg", Imgcodecs.CV_LOAD_IMAGE_GRAYSCALE);
+        Rect rect = new Rect(10, 10, 200, 200);
+        //tl:topleft,br:bottomright,左上角和右下角两个点确定了就好办了,以灰度图加载的时候Scalar中的3个参数要改成一个了,因为是单通道了new Scalar(255)
+        Imgproc.rectangle(src, rect.tl(), rect.br(), new Scalar(255), 2, Imgproc.LINE_8, 0);
+        Imgcodecs.imwrite("D:\\l00379880\\GithubProjects\\images\\lena_result_gray.jpg", src);
+        src.release();
+    }
+}

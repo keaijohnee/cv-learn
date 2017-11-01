2a1f445bb454cd6ae5b14b43cf0c59db5bc6eb01
19920625lsg <1648266192@qq.com>
2017-11-01 11:51:15 +0800

    第四讲--图像遍历和取反3,先用cvtCOlor函数转换为灰度图,然后一次性获取所有的像素

--- opencv4java/src/main/java/com/huawei/l00379880/Lesson04MatForeachOnceGray.java
+++ opencv4java/src/main/java/com/huawei/l00379880/Lesson04MatForeachOnceGray.java
@@ -4,11 +4,12 @@ import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
 
 /**
  * 一次性遍历图像,获取像素点与图像的额大小和类型等相关信息
  */
-public class Lesson04MatForeachOnce {
+public class Lesson04MatForeachOnceGray {
     public static void main(String[] args) {
         String rootPath = "D:\\l00379880\\GithubProjects\\images\\";
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
@@ -33,23 +34,23 @@ public class Lesson04MatForeachOnce {
         int depth = src.depth();
         System.out.println("depth:" + depth);
         System.out.println("***************************图像的类型转换****************************");
-        // 转换为浮点图像,进行位数扩展,方便进行大图像精确计算
-        Mat dst = new Mat(src.size(), CvType.CV_32FC1);
-        src.convertTo(dst, CvType.CV_32F);
-        System.out.println("转换完成");
+        // 转换为灰度图像,下面的函数是非常常用的
+        Mat dst = new Mat();
+        // 把三色图像转换为灰度图
+        Imgproc.cvtColor(src, dst, Imgproc.COLOR_BGR2GRAY);
+        channels = dst.channels();
 
         System.out.println("***************************循环遍历图像******************************");
         // 读取并修改每个像素点的像素值
         // 用于获取全部的图像数据
         byte[] data = new byte[channels * width * height];
         //把全部元素放到data中
-        src.get(0, 0, data);
+        dst.get(0, 0, data);
         int r, g, b;
         //灰度图像只有一个通道
         int gray;
         for (int row = 0; row < height; row++) {
             for (int col = 0; col < width; col++) {
-                // 赋值给pixel
                 if (channels == 3) {
                     // 彩色图像,&0xff是为了转换为十进制
                     b = data[(row * width + col) * channels];
@@ -75,8 +76,8 @@ public class Lesson04MatForeachOnce {
             }
         }
         // 把点处理完了就全放回去
-        src.put(0, 0, data);
-        Imgcodecs.imwrite(rootPath + "lesson04_changePixels_Once.jpg", src);
+        dst.put(0, 0, data);
+        Imgcodecs.imwrite(rootPath + "lesson04_changePixels_Once_Gray.jpg", dst);
         System.out.println("图像转换完成");
         src.release();
         dst.release();

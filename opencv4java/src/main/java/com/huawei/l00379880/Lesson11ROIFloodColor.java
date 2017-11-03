0bfe1d99827470ab1d34b188d25f140abaf4bc16
19920625lsg <1648266192@qq.com>
2017-11-03 21:55:05 +0800

    第11讲完毕了,泛洪填充完全学完了

---
+++ opencv4java/src/main/java/com/huawei/l00379880/Lesson11ROIFloodColor.java
@@ -0,0 +1,32 @@
+package com.huawei.l00379880;
+/**
+ * 泛洪填充,第一个函数,可以填充彩色图像
+ */
+
+import org.opencv.core.*;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+
+public class Lesson11ROIFloodColor {
+    public static void main(String[] args) {
+        String rootPath = "D:\\l00379880\\GithubProjects\\images\\";
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        Mat src = Imgcodecs.imread(rootPath + "dilireba.png");
+        ImageUI ui = new ImageUI();
+        ui.imshow("lena", src);
+
+
+        Rect rect = new Rect(1, 1, src.cols(), src.rows());
+        // 遮罩必须比原图像大两个像素.只能对二值图像进行填充
+        Mat mask = Mat.zeros(src.rows() + 2, src.cols() + 2, CvType.CV_8UC1);
+        // 下面的语句是核心,即对指定的区域开始泛洪填充,Point点的选择至关重要,下面是两个典型 FLOODFILL_MASK_ONLY 也是最后一个参数的可选类型,
+        // 但是只对遮罩起作用,所以不太常用
+        Imgproc.floodFill(src, mask, new Point(2, 2), new Scalar(0, 0, 255), rect,
+                new Scalar(100, 100, 100), new Scalar(100, 100, 100), Imgproc.FLOODFILL_FIXED_RANGE);
+        // 填充人像区域
+        Imgproc.floodFill(src, mask, new Point(src.cols() / 2, src.rows() / 2), new Scalar(0, 255, 255), rect,
+                new Scalar(100, 100, 100), new Scalar(100, 100, 100), Imgproc.FLOODFILL_FIXED_RANGE);
+        ImageUI resultUI = new ImageUI();
+        resultUI.imshow("填充后的图", src);
+    }
+}

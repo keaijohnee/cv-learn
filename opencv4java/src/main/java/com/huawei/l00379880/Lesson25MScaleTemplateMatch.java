4ccb9379202d186269873794c8a501c67916fc8b
19920625lsg <1648266192@qq.com>
2017-11-07 11:08:51 +0800

    第25讲初步3

--- opencv4java/src/main/java/com/huawei/l00379880/Lesson25MScaleTemplateMatch.java
+++ opencv4java/src/main/java/com/huawei/l00379880/Lesson25MScaleTemplateMatch.java
@@ -20,39 +20,51 @@ public class Lesson25MScaleTemplateMatch {
         Mat src = Imgcodecs.imread(rootPath + "test1.png");
         Mat template = Imgcodecs.imread(rootPath + "tpl.png");
 
+        // 三层高斯,获取各个层次的图
+        List<Mat> scaleList = buildGaussianPyramid(src, 3);
+        List<Mat> templateList = buildGaussianPyramid(template, 3);
+        // TM_XXX下面有5中选择,但是TM_CCOEFF_NORMED的效果最好
+        int method = Imgproc.TM_CCOEFF_NORMED;
+        for (int i = 0; i < scaleList.size(); i++) {
+            for (int j = 0; j < templateList.size(); j++) {
+                Core.MinMaxLocResult minMaxLocResult = matchImage(scaleList.get(i), templateList.get(j), method);
+
+                Point maxLoc = minMaxLocResult.maxLoc;
+                Point minLoc = minMaxLocResult.minLoc;
+
+                Point matchLoc = null;
+                // Imgproc.TM_XX下五中方法的比较
+                if (method == Imgproc.TM_SQDIFF || method == Imgproc.TM_SQDIFF_NORMED) {
+                    matchLoc = minLoc;
+                } else {
+                    matchLoc = maxLoc;
+                }
+                // 为了不影响原图,所以先clone一份给自己
+                Mat copy = src.clone();
+                Imgproc.rectangle(copy, matchLoc, new Point(matchLoc.x + template.cols(), matchLoc.y + template.rows()), new Scalar(0, 0, 255), 2, 8, 0);
+                ImageUI matchUI = new ImageUI();
+                matchUI.imshow("把匹配的结果在原图上显示出来", copy);
+            }
+        }
+
+    }
+
+    /**
+     * 返回图像的匹配点
+     *
+     * @param src      原始图片
+     * @param template 模板图片
+     * @return 返回匹配位置的最大和最小点的封装数据类型MinMaxLocResult
+     */
+    public static Core.MinMaxLocResult matchImage(Mat src, Mat template, int method) {
         //下面注意:模板匹配的结果result的大大小必须是(原图片-模板大小+1),长款都必须是这个原则,否则无法运行
         int height = src.rows() - template.rows() + 1;
         int width = src.cols() - template.cols() + 1;
         Mat result = new Mat(height, width, CvType.CV_32FC1);
-
-        // TM_XXX下面有5中选择,但是TM_CCOEFF_NORMED的效果最好
-        int method = Imgproc.TM_CCOEFF_NORMED;
-
         // 开始匹配,并制定匹配方法
         Imgproc.matchTemplate(src, template, result, method);
         Core.MinMaxLocResult minMaxLocResult = Core.minMaxLoc(result);
-        Point maxLoc = minMaxLocResult.maxLoc;
-        Point minLoc = minMaxLocResult.minLoc;
-
-        Point matchLoc = null;
-        // Imgproc.TM_XX下五中方法的比较
-        if (method == Imgproc.TM_SQDIFF || method == Imgproc.TM_SQDIFF_NORMED) {
-            matchLoc = minLoc;
-        } else {
-            matchLoc = maxLoc;
-        }
-
-        Mat result8u = new Mat();
-        Core.normalize(result, result, 0, 255, Core.NORM_MINMAX);
-        result.convertTo(result8u, CvType.CV_8UC1);
-        ImageUI result8uUI = new ImageUI();
-        result8uUI.imshow("匹配结果的灰度图,右下角的点为开始匹配的点", result8u);
-
-        // 为了不影响原图,所以先clone一份给自己
-        Mat copy = src.clone();
-        Imgproc.rectangle(copy, matchLoc, new Point(matchLoc.x + template.cols(), matchLoc.y + template.rows()), new Scalar(0, 0, 255), 2, 8, 0);
-        ImageUI matchUI = new ImageUI();
-        matchUI.imshow("把匹配的结果在原图上显示出来", copy);
+        return minMaxLocResult;
     }
 
     /**

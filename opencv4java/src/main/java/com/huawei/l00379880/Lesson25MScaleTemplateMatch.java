e801a9edf7bfed36c42210aef77341fc5f2b14b9
19920625lsg <1648266192@qq.com>
2017-11-07 11:15:47 +0800

    第25讲初步5

--- opencv4java/src/main/java/com/huawei/l00379880/Lesson25MScaleTemplateMatch.java
+++ opencv4java/src/main/java/com/huawei/l00379880/Lesson25MScaleTemplateMatch.java
@@ -25,8 +25,13 @@ public class Lesson25MScaleTemplateMatch {
         List<Mat> templateList = buildGaussianPyramid(template, 3);
         // TM_XXX下面有5中选择,但是TM_CCOEFF_NORMED的效果最好
         int method = Imgproc.TM_CCOEFF_NORMED;
+        boolean found = false;
         for (int i = 0; i < scaleList.size(); i++) {
             for (int j = 0; j < templateList.size(); j++) {
+                // 匹配到就退出
+                if (found) {
+                    break;
+                }
                 Core.MinMaxLocResult minMaxLocResult = matchImage(scaleList.get(i), templateList.get(j), method);
 
                 Point maxLoc = minMaxLocResult.maxLoc;
@@ -45,10 +50,11 @@ public class Lesson25MScaleTemplateMatch {
                 // 匹配度大于0.75才显示出来
                 if (matchValue > 0.75) {
                     // 为了不影响原图,所以先clone一份给自己
-                    Mat copy = src.clone();
-                    Imgproc.rectangle(copy, matchLoc, new Point(matchLoc.x + template.cols(), matchLoc.y + template.rows()), new Scalar(0, 0, 255), 2, 8, 0);
+                    Mat copy = scaleList.get(i).clone();
+                    Imgproc.rectangle(copy, matchLoc, new Point(matchLoc.x + templateList.get(j).cols(), matchLoc.y + templateList.get(j).rows()), new Scalar(0, 0, 255), 2, 8, 0);
                     ImageUI matchUI = new ImageUI();
                     matchUI.imshow("把匹配的结果在原图上显示出来", copy);
+                    found = true;
                     break;
                 }
             }
